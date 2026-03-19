e7605558688a2607c19874401df53d66a9768a80
ujjwalsz <uk8629173@gmail.com>
2026-03-19 07:54:07 +0000

    datatype

---
+++ 01/datatype02.js
@@ -0,0 +1,33 @@
+"use strict";  // yteat all js codde ad newer version
+
+
+//alert("hello")
+
+//alert( 3 + 3) //we are using nodejs so it willl not give result like browser
+
+console.log(3 + 3)
+
+
+
+let name = "hitesh"
+let age =  18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bollean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+
+console.log(typeof "ujjwal");
+
+
+console.log(typeof null)   // object
+
+console.log(typeof undefined) // undefined
