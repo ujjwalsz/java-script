bfd6b9fa521faf7f3a6bb5e738e74ac8325fbeb2
ujjwalsz <uk8629173@gmail.com>
2026-03-30 11:17:45 +0000

    array

---
+++ 01/array.js
@@ -0,0 +1,50 @@
+// array in javascript
+// javascript array are resizeable
+// shallow copy and deep copy
+const myArr = [0,1,2,3,4,5]
+// const myHeroes = ["ironman","captain america"]
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+// array method
+
+// myArr.push(6)
+// myArr.push(7)// simply just number ku push kardeta ha
+// myArr.pop()// simply just last number ku hata deta haa
+
+
+///starting me kisi chiz ku add kardeta haa
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()
+// join array ku bind kardeta ha aur string ma convert bhi kardeta haa
+
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+// slice,spice
+
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)// 
+
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c", myArr);
+console.log(myn2);
+
+// slice ma range include nhi huta(aur array ma change nhi huta)
+// splice ma range include huta ha (aur original array ma hi change hu jata haa)
+
+
+
