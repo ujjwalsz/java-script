e28fd76c645c8870c676a8ce99c57b886165eba1
ujjwalsz <uk8629173@gmail.com>
2026-04-03 20:26:42 +0000

    object destructure

---
+++ 01/object de-structure.js
@@ -0,0 +1,47 @@
+// destructure
+// object ki bhi huta ha aur array ki
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "ujjwal",
+}
+
+//course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// //react
+
+
+// const navbar = ({company}) => {
+
+// }
+
+
+// navbar(company = "hitesh")
+
+// API
+// apna kaam kisi aur par dal deta ha usa hi api kaheta ha
+// json {}
+
+
+// {
+//     "name": "ujjwal",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// api call fetch method
+
+[
+    {},
+    {},
+    {},
+]
+
+
